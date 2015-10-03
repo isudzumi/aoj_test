0a74ec89a9f1920b2b860942119ce84d17872830
isudzumi <isudzumi@gmail.com>
2015-10-03 23:16:12 +0900

    #fifth repository

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
 using namespace std;
 
-void trace(int a[], int n){
-	for(int i = 1; i <= n; i++){
-		if(i > 1){
-			cout << " ";
-		}
-		cout << a[i];
-	}
-	cout << endl;
-}
 
 int main()
 {
-	int n, i, j;
-	int a[100+1];
+	int n, i, j, v, k;
 	
 	cin >> n;
-	for(i = 1; i <=n; i++){
+	int a[n-1];
+	for(i = 0; i < n; i++){
 		cin >> a[i];
 	}
 	
-	trace(a, n);
+	for(i = 0; i < n; i++){
+		v = a[i+1];
+		a[i + 1] = a[i];
+		while(a[j] > v){
+			j++;
+		}
+		for(j; j<a[i];j--){
+			a[j] = a[j+1];
+		}
+		cout << a[i] << " ";
+	}
+	cout << endl;
 	return 0;
 }
